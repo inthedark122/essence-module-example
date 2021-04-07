bb68441deef66aedca7df1765e8f03eaf8d49158
Oleh Kutsenko <inthedark1225@gmail.com>
2021-04-07 22:35:51 +0300

    step-3 use globa values with single global variable

--- src/App.tsx
+++ src/App.tsx
@@ -1,4 +1,5 @@
 import * as React from "react";
+import {useObserver} from "mobx-react";
 import {IClassProps} from "@essence-community/constructor-share/types";
 import Card from "@material-ui/core/Card";
 import CardContent from "@material-ui/core/CardContent";
@@ -7,12 +8,15 @@ import CardActions from "@material-ui/core/CardActions";
 import Button from "@material-ui/core/Button";
 
 const App: React.FC<IClassProps> = (props: IClassProps) => {
-    const {bc} = props;
+    const {bc, pageStore} = props;
 
-    return (
+    return useObserver(() => (
         <Card>
             <CardContent>
                 <Typography>Глобальная переменная: {bc.getglobal}</Typography>
+                {bc.getglobal ? (
+                    <Typography>Глобальное значение: {pageStore.globalValues.get(bc.getglobal)}</Typography>
+                ) : null}
             </CardContent>
             <CardActions>
                 <Button size="small" color="primary">
@@ -20,7 +24,7 @@ const App: React.FC<IClassProps> = (props: IClassProps) => {
                 </Button>
             </CardActions>
         </Card>
-    );
+    ));
 };
 
 export default App;
